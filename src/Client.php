7e1fdb6be20e85dab10dccca3116dfa888a5c933
Emil Kilhage <emil.kilhage@glooby.com>
2016-10-15 13:10:34 +0000

    Added initial version

---
+++ src/Client.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Glooby\Pexels;
+
+/**
+ * @author Emil Kilhage <emil@glooby.com>
+ */
+class Client
+{
+    /**
+     * @var string
+     */
+    private $token;
+
+    /**
+     * @var \GuzzleHttp\Client
+     */
+    private $client;
+
+    /**
+     * @param string $token
+     */
+    public function __construct($token)
+    {
+        $this->token = $token;
+    }
+
+    /**
+     * @return \GuzzleHttp\Client
+     */
+    private function getClient()
+    {
+        if (null === $this->client) {
+            $this->client = new Client([
+                'defaults' => [
+                    'headers' => [
+                        'Authorization' => $this->token
+                    ]
+                ]
+            ]);
+        }
+
+        return $this->client;
+    }
+
+    /**
+     * @param string $query
+     * @param int $size
+     * @param int $page
+     *
+     * @return \GuzzleHttp\Message\ResponseInterface
+     */
+    public function search($query, $size = 15, $page = 1)
+    {
+        return $this->getClient()->get('http://api.pexels.com/v1/search?'.http_build_query([
+            'query' => $query,
+            'per_page' => $size,
+            'page' => $page
+        ]));
+    }
+}
